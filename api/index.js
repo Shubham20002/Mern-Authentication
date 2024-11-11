8bf7c793266e44a19601977c6d227d504299dfe7
shubham <shubham992284@gmail.com>
2024-11-11 21:28:39 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import userRouter from '../api/routes/user.route.js'
 import authRouter from '../api/routes/auth.route.js'
-
+import path from 'path';
 configDotenv();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to db");
 }).catch((err)=>{
     console.log("error",err)
 })
-
+const __dirname = path.resolve();
 const app=express();
 app.use(express.urlencoded())
 app.use(express.json());
@@ -21,6 +21,10 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
