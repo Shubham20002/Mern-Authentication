284c1a4fbb6bd6ee5055847b14f9955ff7981a8a
shubham <shubham992284@gmail.com>
2024-11-11 21:39:19 +0530

    heading changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,8 +25,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Patil</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>Auth</span>
+            {/* <span className='text-slate-700'>Estate</span> */}
           </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
